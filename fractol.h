df41218abfbdf4d7e469f7fb6d5854981eafcf09
granger-vibe <milsanch@student.42madrid.com>
2025-03-30 18:40:28 +0200

    library updated

--- fractol.h
+++ fractol.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: milsanch <milsanch@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: milagros <milagros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 13:30:07 by milsanch          #+#    #+#             */
-/*   Updated: 2025/03/29 13:30:14 by milsanch         ###   ########.fr       */
+/*   Updated: 2025/03/30 18:40:09 by milagros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,17 @@
 
 #define ERROR_MESSAGE "Error, enter the fractal's name, it can be mandelbrot or julia " //revisar sintaxis
 
+typedef struct	s_img
+{
+	void	*img; //pointer to image struct
+	char	*pixels // points to the actual pixels
+}
 
+typedef struct	s_fract
+{
+	void	*mlx
+
+}
 typedef struct	complex_type
 {
 	double	real;
